6ad8f39de838c626edce221bae1abab9f7e3c474
Asdrubal Ivan Suarez Rivera <asdrubal.ivan.suarez.rivera@gmail.com>
2020-09-08 15:35:27 -0300

    Fixing images

--- resources/image.py
+++ resources/image.py
@@ -1,7 +1,9 @@
 from flask_restful import Resource
 from flask_uploads import UploadNotAllowed
-from flask import request
+from flask import request, send_file
 from flask_jwt_extended import jwt_required, get_jwt_identity
+import traceback
+import os
 
 from libs import image_helper
 from libs.strings import gettext
@@ -27,4 +29,26 @@ class ImageUpload(Resource):
             return {"message": gettext("image_uploaded").format(basename)}, 201
         except UploadNotAllowed:
             extension = image_helper.get_extension(data["image"])
-            return {"message": gettext("image_illegal_extension").format(extension)}, 400
+            return (
+                {"message": gettext("image_illegal_extension").format(extension)},
+                400,
+            )
+
+
+class Image(Resource):
+    @jwt_required
+    def get(self, filename: str):
+        """Returns the requested image in the user's folder only"""
+        user_id = get_jwt_identity()
+        folder = f"user_{user_id}"
+        if not image_helper.is_filename_safe(filename):
+            return {"message": gettext("illegal_filename").format(filename)}
+        try:
+            print(image_helper.get_path(filename, folder=folder))
+            return send_file(image_helper.get_path(filename, folder=folder)) # No se pone 200 porque tira un error
+        except FileNotFoundError:
+            return {"message": gettext("image_not_found").format(filename)}, 404
+
+    @jwt_required
+    def delete(self, filename: str):
+        pass
